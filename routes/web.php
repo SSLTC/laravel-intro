dfb9587f7f7b237b2c750ecce5d857dff98cd99e
Sven Snoeck <snoeck.sven@live.be>
2023-03-27 13:55:14 +0200

    Two linked pages via routes/controllers/views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [HomeController::class, 'show'])->name('home');
+Route::get('/contact', [ContactController::class, 'show'])->name('contact');
